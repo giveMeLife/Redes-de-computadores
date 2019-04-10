9c8d4f286a2b5eec17866c430dc82c39ea7097d5
giveMeLife <j.fernandez.munoz@hotmail.com>
2019-04-10 09:33:29 -0400

    El gráfico simple

--- lab1/wav.py
+++ lab1/wav.py
@@ -1,6 +1,12 @@
-import numpy
-import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
 import scipy.io.wavfile as wavfile
 
-sound = wavfile.read("handel.wav");
-print(sound[1])
+sound = wavfile.read("handel.wav")
+print(sound[1].size)
+x = np.linspace(0,sound[1].size/sound[0],sound[1].size)
+y=sound[1]
+plt.plot(x,y)
+plt.xlabel("Tiempo (s)")
+plt.ylabel("Muestra")
+plt.show()
