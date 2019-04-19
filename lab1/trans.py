fb034f0c3428c337df2f4d414943edcd8faf90a2
giveMeLife <j.fernandez.munoz@hotmail.com>
2019-04-19 19:14:46 -0400

    comentado2

--- lab1/trans.py
+++ lab1/trans.py
@@ -3,36 +3,54 @@ import matplotlib.pyplot as py
 import scipy.io.wavfile as wavfile
 from scipy.fftpack import fft
 
-############### Grafico de Tiempo ############################
+#Descripción: Función que permite leer un archivo de sonido .wav en el directorio del programa
+#Entradas: Un string que es el nombre del archivo a leer
+#Salida: Un arreglo donde su primer elemento es la cantidad de muestras obtenidas en un segundo
+#		 y el segundo elemento es un arreglo con el total de muestras obtenidas del archivo.
 def readFile(nombre):
 	sound = wavfile.read(nombre)
 	return sound
 
+#Gráfico de amplitud v/s tiempo
+#Descripción: Función que grafica una muestra de amplitudes de un sonido en el tiempo
+#Entrada: Un arreglo donde su primer elemento es la cantidad de muestras obtenidas en un segundo
+#  		  y el segundo elemento es un arreglo con el total de muestras obtenidas del archivo.
+#Salida: 
 def graficoTiempo(sound):
 	x = np.linspace(0,sound[1].size/sound[0],sound[1].size)
 	y=sound[1]
-	py.figure(1)
+	'''py.figure(1)
 	py.plot(x,y,'b')
 	py.title("Amplitud v/s Tiempo")
 	py.xlabel("Tiempo (s)")
 	py.ylabel("Amplitud")
-	py.title("Funcion en el Tiempo original")
+	py.title("Funcion en el Tiempo original")'''
 
-################ Grafico de Frecuencia #######################
+#Grafico Transformada Fourier 
+#Descripción: Función que realiza una transformada de Fourier un set de datos correspondientes a la amplitud de un sonido
+#             , calcula la frecuencia sobre la que se proyectan y genera el gráfico
+#Entrada: Un arreglo donde su primer elemento es la cantidad de muestras obtenidas en un segundo
+#  		  y el segundo elemento es un arreglo con el total de muestras obtenidas del archivo.
+#Salida: Un arreglo con la transformada de Fourier de las muestras de un sonido y otro arreglo con
+#		 la frecuencia sobre la que se proyectan
 def frecuenciaFourier(sound):
 	transformada = np.fft.fft(sound[1])
 	freq = np.fft.fftfreq(sound[1].size,1/sound[0])
-	py.figure(2)
+	'''py.figure(2)
 	py.plot(freq,np.absolute(transformada),'g')
 	py.title("Amplitud v/s Frecuencia")
 	py.xlabel('Frecuencia')
 	py.ylabel('Amplitud')
-	#py.show()
+	#py.show()'''
 	return transformada,freq
 
-################ Grafico de Frecuencia truncado #######################
+#Transformada de Fourier truncada
+#Descripción: Función que trunca ciertos valores de una transformada de Fourier a 0.
+#Entrada: Un arreglo con los datos obtenidos al realizar la transformada de Fourier del sonido
+#Salida: Arreglo con datos truncados a 0 del arreglo de entrada.
 def frecuenciaFourierTruncado(transformada):
 	copyT = transformada.copy()
+	freq = np.fft.fftfreq(sound[1].size,1/sound[0])
 	for x in range(2000):
 		copyT[x] = 0
 		copyT[73112 - x] = 0
@@ -40,28 +58,36 @@ def frecuenciaFourierTruncado(transformada):
 		copyT[x] = 0
 		copyT[73112 - x] = 0
 	#py.subplot(4,1,3)
-	py.figure(3)
+	'''py.figure(3)
 	py.plot(freq,np.absolute(copyT),'m')
 	py.title("Transformada De Fourier truncada")
 	py.xlabel('Frecuencia(Hz)')
-	py.ylabel('Amplitud')
+	py.ylabel('Amplitud')'''
 	#py.show()
 	return copyT
 
-################ Grafico de Frecuencia truncado inversa #######################
+#Gráfico transformada de Fourier inversa truncada y genera un archivo de sonido nuevo
+#Descripción: Función que obtiene la transformada de Fourier inversa de un arreglo de datos
+#Entrada: Un arreglo con un set de datos correspondiente a la transformada de Fourier truncada y
+# 		  un arreglo donde su primer elemento es la cantidad de muestras obtenidas en un segundo
+#  		  y el segundo elemento es un arreglo con el total de muestras obtenidas del archivo.
+#Salida: Un arreglo con los valores de la transformada de Fourier inversa truncada
+
 def inversaTruncada(copyT, sound):
 	inversa = np.fft.ifft(copyT).real
 	x = np.linspace(0,sound[1].size/sound[0],sound[1].size)
-	py.figure(4)
+	'''py.figure(4)
 	py.plot(x,inversa,'r')
 	py.title("Transformada inversa de Fourier truncada")
 	py.xlabel('Tiempo')
 	py.ylabel('Amplitud')
-	py.show()
+	py.show()'''
 	wavfile.write("test.wav",sound[0],inversa)
 	return inversa
 
-
+#Descripción: Función que calcula el error cuadrático medio entre dos arreglos
+#Entrada: Dos arreglos con datos de largo 73113
+#salida: Error cuadrático medio de los dos arreglos.
 def errorCuadratico(real,calculado):
 	sumatoria = 0
 	for x in range(73113):
@@ -72,6 +98,7 @@ def errorCuadratico(real,calculado):
 	return raiz
 
 
+#################################BLOQUE PRINCIPAL#######################################
 sound = readFile("handel.wav")
 graficoTiempo(sound)
 transformada, freq = frecuenciaFourier(sound)
