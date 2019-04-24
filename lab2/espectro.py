def857355a0b4fa332cef016dcf89c9802359460
Vicente Vega <vicente.vega@usach.cl>
2019-04-24 09:31:41 -0400

    agregado el espectograma

--- lab2/espectro.py
+++ lab2/espectro.py
@@ -9,7 +9,9 @@ def readFile(nombre):
 
 sound = readFile("handel.wav")
 freq, time, espec = signal.spectrogram(sound[1],sound[0])
-plt.pcolormesh(time,freq,espec)
-plt.ylabel("Frecuencia")
-plt.xlabel("Tiempo")
+test = np.log10(espec)
+im = plt.pcolormesh(time,freq,test)
+plt.ylabel("Frecuencia [Hz]")
+plt.xlabel("Tiempo [s]")
+plt.colorbar(im).set_label('Intensidad [dB]')
 plt.show()
