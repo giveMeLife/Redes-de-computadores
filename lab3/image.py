e3762bae21bd90c9b70689143e605e923079ceae
giveMeLife <j.fernandez.munoz@hotmail.com>
2019-05-08 01:17:23 -0400

    lectura imagen y creacion array

---
+++ lab3/image.py
@@ -0,0 +1,25 @@
+from scipy.misc import imread
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as imgplt
+
+imagen = imread("leena512.bmp",flatten=False,mode='RGB');
+kernel = np.array([ [ 1, 4, 6 ,4, 1],
+                    [ 4, 16, 24, 16, 4],
+                    [ 6, 24, 36, 24, 6],
+                    [ 4, 16, 24, 16, 4],
+                    [ 1, 4, 6, 4, 1]])
+
+kernel = np.multiply(kernel,1/256)
+
+aux = np.zeros((512,512,3),dtype=np.uint8)
+for i in range(2,512):
+    for j in range(2, 512):
+        aux[i][j][0] = imagen[i][j][0]
+        aux[i][j][1] = imagen[i][j][1]
+        aux[i][j][2] = imagen[i][j][2]
+
+imgplot = plt.imshow(aux)
+plt.show()
+#print(imagen[0][0])
+
