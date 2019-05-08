ce381d2c6ddf140d3d13e15de175710f9f9788b8
giveMeLife <j.fernandez.munoz@hotmail.com>
2019-05-08 09:04:57 -0400

    bordes

--- lab3/prueba.py
+++ lab3/prueba.py
@@ -1,12 +1,13 @@
 from scipy.misc import imread
 import numpy as np
+from PIL import Image
 import matplotlib.pyplot as plt
 import matplotlib.image as imgplt
-from PIL import Image
 
 imagen = Image.open("leena512.bmp")
-matrizImagen = np.array(imagen)
-print(matrizImagen)
+
+img = np.array(imagen)
+
 kernel = np.array([ [ 1, 4, 6 ,4, 1],
                     [ 4, 16, 24, 16, 4],
                     [ 6, 24, 36, 24, 6],
@@ -15,12 +16,13 @@ kernel = np.array([ [ 1, 4, 6 ,4, 1],
 
 kernel = np.multiply(kernel,1/256)
 
-aux = np.zeros((512,512,3),dtype=np.uint8)
-for i in range(2,512):
-    for j in range(2, 512):
-        aux[i][j][0] = imagen[i][j][0]
-        aux[i][j][1] = imagen[i][j][1]
-        aux[i][j][2] = imagen[i][j][2]
+aux = np.zeros((np.shape(img)[0]+2,np.shape(img)[1]+2),dtype=np.uint8)
+for i in range(2,np.shape(aux)[0]):
+    for j in range(2, np.shape(aux)[1]):
+        aux[i][j] = img[i-2][j-2]
 
-#print(imagen[0][0])'''
 
+print(np.shape(img))
+#imgplot = plt.imshow(imagen)
+Image.fromarray(aux).show()
+#print(imagen[0][0])
