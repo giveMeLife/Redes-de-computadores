75243c6fd96b5f683051f7ce9773ca4c6b4f778e
Vicente Vega <vicente.vega@usach.cl>
2019-06-05 08:51:52 -0400

    leer handle

--- lab4/modulacion.py
+++ lab4/modulacion.py
@@ -1,3 +1,10 @@
 import numpy as np 
 import matplotlib.pyplot as py
 import scipy.io.wavfile as wavfile
+
+def readFile(nombre):
+	sound = wavfile.read(nombre)
+	return sound
+
+data = readFile("handel.wav")
+
