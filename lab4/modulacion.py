72b3e59712d3992533d21751ce0831757cefd6e7
Vicente Vega <vicente.vega@usach.cl>
2019-06-18 14:53:15 -0400

    arreglo en modulacion AM y FM, mas graficos

--- lab4/modulacion.py
+++ lab4/modulacion.py
@@ -1,92 +1,93 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 import scipy.io.wavfile as wavfile
+from scipy import signal
 from scipy import interpolate , integrate
 
 def readFile(nombre):
 	sound = wavfile.read(nombre)
 	return sound
 
-def integral(data,rate):
-	integral_array = []
-	dataLen = len(data)
-	delta = 1/rate
-	time = np.linspace(0,(dataLen*1)*delta,dataLen)
-	for i  in range(len(data)):
-		integral_array.append(integrate.simps(data[:i+1],dx=delta))
-	return integral_array
-
-def interpolation(rate,data,time):
+def interpolation(rate,data):
+	time = np.linspace(0,len(data)/rate,len(data))
 	interpol = interpolate.interp1d(time,data)
-	time_interpol = np.linspace(0,data.size/rate,data.size*3)
+	time_interpol = np.linspace(0,len(data)/rate,len(data))
 	data_interpol = interpol(time_interpol)
-	return data_interpol,time_interpol
+	return data_interpol
 
-def modulationAM(data_interpol,rate,time_interpol,k):
+def modulationAM(data,data_interpol,rate,k):
 	fc = 3*rate
+	time_interpol = np.linspace(0,len(data_interpol)/rate,num=len(data_interpol))
+	time = np.linspace(0,len(data)/rate,num=len(data))
 	coseno = np.cos(2*np.pi*fc*time_interpol)
 	result = k * data_interpol * coseno
+	
+
 	return result,coseno
 
-def modulationFM(data_interpol,rate,time_interpol,k):
-	##integral = np.cumsum(data_interpol) / rate
-	integral_arr = integral(data_interpol,rate)
+def modulationFM(data,data_interpol,rate,k):
+	time_interpol = np.linspace(0,len(data_interpol)/rate,num=len(data_interpol))
+	time = np.linspace(0,len(data)/rate,num=len(data))
+	integral = np.cumsum(data_interpol) /rate
 	fc = 3*rate
 	first_term = 2*np.pi*fc*time_interpol
-	coseno = np.cos(first_term + k * integral_arr)
-	return coseno
+	coseno = np.cos(first_term)
+	result = np.cos(first_term + k * integral)
+	return result,coseno
 
+def demodulationAM(data_modulation,rate):
+	fc = 3*rate
+	time_interpol = np.linspace(0,len(data_modulation)/rate,num=len(data_modulation))
+	coseno = np.cos(2*np.pi*fc*time_interpol)
+	result = data_modulation * coseno
+	return result
 
+def filtradoBajo(rate,data):
+	b, a = signal.butter(3,4000,'low',fs=rate)
+	y = signal.filtfilt(b, a, data)
+	transformada2 = np.fft.fft(y).real
+	wavfile.write("filtradoBajo.wav",rate,y)
+	return y,transformada2
+
+def graphicAM(data,data_interpol,coseno,result):
+	time_interpol = np.linspace(0,len(data_interpol)/rate,num=len(data_interpol))
+	time = np.linspace(0,len(data)/rate,len(data))
+	plt.subplot(3,1,1)
+	plt.plot(time,data,'b')
+	plt.subplot(3,1,2)
+	plt.plot(time_interpol,coseno,'g')
+	plt.subplot(3,1,3)
+	plt.plot(time_interpol,result,'m')
+	plt.show()
+
+def graphicFM(data,data_interpol,coseno,result):
+	time_interpol = np.linspace(0,len(data_interpol)/rate,num=len(data_interpol))
+	time = np.linspace(0,len(data)/rate,len(data))
+	plt.subplot(3,1,1)
+	plt.plot(time,data,'b')
+	plt.subplot(3,1,2)
+	plt.plot(time_interpol,coseno,'g')
+	plt.subplot(3,1,3)
+	plt.plot(time_interpol,result,'m')
+	plt.show()
+
+def graphicFourier(data,rate,color):
+	transData = np.fft.fft(data).real
+	freq = np.fft.fftfreq(len(data),1/rate)
+	plt.plot(freq,transData,color)
+	plt.show()
 
 rate,data = readFile("handel.wav")
-frecuency = data.size / rate
-time = np.linspace(0,data.size/rate,data.size)
-data_interpol,time_interpol = interpolation(rate,data,time)
-data_interpol2,time_interpol2 = interpolation(rate,data,time)
-dataAM,portadora = modulationAM(data_interpol,rate,time_interpol,1)
-dataFM = modulationFM(data_interpol2,rate,time_interpol2,1)
-time_portadora = np.linspace(0,portadora.size/rate,portadora.size)
+data_interpol = interpolation(rate,data)
+dataAM,portadoraAM = modulationAM(data,data_interpol,rate,1)
+dataDemodulationAM = demodulationAM(dataAM,rate)
+y,trans = filtradoBajo(rate,dataDemodulationAM)
+dataFM,portadoraFM = modulationFM(data,data_interpol,rate,1)
+graphicAM(data,data_interpol,portadoraAM,dataAM)
+graphicFM(data,data_interpol,portadoraFM,dataFM)
 
-plt.subplot(4,1,1)
-plt.plot(time,data)
-plt.subplot(4,1,2)
-plt.plot(time_interpol,dataAM)
-plt.subplot(4,1,3)
-plt.plot(time_portadora,portadora)
-plt.subplot(4,1,4)
-plt.plot(time_interpol,dataFM)
-plt.show()
-
-'''transData = np.fft.fft(data)
-freq = np.fft.fftfreq(data.size,1/rate)
-transDataAM = np.fft.fft(dataAM)
-freqAM = np.fft.fftfreq(dataAM.size,1/rate)
-transDataFM = np.fft.fft(dataFM)
-freqFM = np.fft.fftfreq(dataFM.size,1/rate)
-
-plt.subplot(3,1,1)
-plt.plot(freq,transData)
-plt.subplot(3,1,2)
-plt.plot(freqAM,transDataAM)
-plt.subplot(3,1,3)
-plt.plot(freqFM,transDataFM)
-plt.show()'''
-
-
-'''interpolada = interpolate.interp1d(time,sound[1])
-time2 = np.linspace(0,sound[1].size/sound[0],sound[1].size*3)
-sound2 = interpolada(time2)
-frecuencia = sound[1].size/sound[0]
-time_interpolada = np.linspace(0,sound2.size/sound[0],sound2.size)
-
-###########Modulacion AM########################
-coseno = np.cos(2*np.pi*frecuencia*time_interpolada)
-result = sound2 * coseno
-#################################################
-
-#integral = np.cumsum(sound2) / frecuencia
-
-###########Modulacion FM#########################
-integral = np.cumsum(sound2)
-cosenoFM = np.cos(2*np.pi*frecuencia*time_interpolada + integral)'''
+graphicFourier(data,rate,'b')
+graphicFourier(dataAM,rate,'m')
+graphicFourier(dataFM,rate,'y')
+graphicFourier(dataDemodulationAM,rate,'r')
 
