a0aef33894f08d75c24ceec2d5151116a865e71b
Vicente Vega <vicente.vega@usach.cl>
2019-06-21 19:19:17 -0400

    arreglo de graficos

--- lab4/modulacion.py
+++ lab4/modulacion.py
@@ -1,8 +1,7 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 import scipy.io.wavfile as wavfile
-from scipy import signal
-from scipy import interpolate
+from scipy import interpolate, signal
 
 #Entrada: Nombre del archivo 
 #Proceso: Lee la informacion del archivo de audio. 
@@ -126,77 +125,84 @@ def Fourier(data,rate):
 	freq = np.fft.fftfreq(len(data),1/rate)
 	return transData,freq
 
-#Function:
-#Process:
-#Return:
-def graphicFourier(data,rate,dataAM,rateAM,dataFM,rateFM,dataDemodulada,percentage):
+#Entrada: Recibe los datos de la señal original, la informacion de las señales moduladas, la informacion de la señal demodulada, la señal portadora 
+#		  ademas de la frecuencia de cada uno y una bandera para indicar si se grafica FM o AM.
+#Proceso: Grafica cada informacion segun lo que indique la bandera. Si es AM se realizan 4 graficos, en caso contrario solo se realizan 3.
+def graphicFourier(data,rate,dataAM,rateAM,dataFM,rateFM,dataDemodulada,carrier,percentage,flag):
 	transData,freq = Fourier(data,rate)
-	transDataAM,freqAM = Fourier(dataAM,rateAM)
-	transDataFM,freqFM = Fourier(dataFM,rateFM)
-	transDatademodulada,freqDemodulada = Fourier(dataDemodulada,rate)
-	fc = 3*rate
-	time_interpol = np.linspace(0,len(data)/rate,num=len(data))
-	coseno = np.cos(2*np.pi*fc*time_interpol)
-	trasnP,freqP = Fourier(coseno,fc)
+	transCarrier,freqCarrier = Fourier(carrier,rateAM)
 	fig = plt.figure()
 	fig.subplots_adjust(hspace=0.7, wspace=0.7)
-	fig.add_subplot(411)
-	plt.title("Señal Original")
+	i = 4
+	if flag == 1:
+		transDataAM,freqAM = Fourier(dataAM,rateAM)
+		transDatademodulada,freqDemodulada = Fourier(dataDemodulada,rate)
+		fig.add_subplot(413)
+		plt.title("Transformada de Fourier con modulacion AM con "+ percentage)
+		plt.xlabel("Frecuencia [Hz]")
+		plt.ylabel("Amplitud [dB]")
+		plt.plot(freqAM,abs(transDataAM),'g')
+		fig.add_subplot(414)
+		plt.title("Transformada de Fourier señal demodulada al "+ percentage)
+		plt.xlabel("Frecuencia [Hz]")
+		plt.ylabel("Amplitud [dB]")
+		plt.plot(freqDemodulada,abs(transDatademodulada),'y')
+	else:
+		i = 3
+		transDataFM,freqFM = Fourier(dataFM,rateFM)
+		fig.add_subplot(313)
+		plt.title("Transformada de Fourier con modulacion FM al "+ percentage)
+		plt.xlabel("Frecuencia [Hz]")
+		plt.ylabel("Amplitud [dB]")
+		plt.plot(freqFM,abs(transDataFM),'r')
+	
+	fig.add_subplot(i,1,1)
+	plt.title("Transformada de Fourier de la señal original")
 	plt.xlabel("Frecuencia [Hz]")
 	plt.ylabel("Amplitud [dB]")
 	plt.plot(freq,abs(transData),'b')
-	plt.title("Señal Portadora")
+	fig.add_subplot(i,1,2)
+	plt.title("Transformada de Fourier de  la señal portadora")
 	plt.xlabel("Frecuencia [Hz]")
 	plt.ylabel("Amplitud [dB]")
-	plt.plot(freqP,abs(trasnP),'g')
-	fig.add_subplot(412)
-	plt.title("Transformada de Fourier con modulacion AM con "+ percentage)
-	plt.xlabel("Frecuencia [Hz]")
-	plt.ylabel("Amplitud [dB]")
-	plt.plot(freqAM,abs(transDataAM),'g')
-	fig.add_subplot(413)
-	plt.title("Transformada de Fourier con modulacion FM al "+ percentage)
-	plt.xlabel("Frecuencia [Hz]")
-	plt.ylabel("Amplitud [dB]")
-	plt.plot(freqFM,abs(transDataFM),'r')
-	fig.add_subplot(414)
-	plt.title("Transformada de Fourier señal demodulada al "+ percentage)
-	plt.xlabel("Frecuencia [Hz]")
-	plt.ylabel("Amplitud [dB]")
-	plt.plot(freqDemodulada,abs(transDatademodulada),'y')
+	plt.plot(freqCarrier,abs(transCarrier),'m')
 	plt.show()
 
+#Lectura e interpolacion de la informacion
 rate,data = readFile("handel.wav")
 data_interpol = interpolation(rate,data)
 
 ####################### MODULACION 100%#######################################
-dataAM_1,portadoraAM_1,rateAM_1 = modulationAM(data_interpol,rate,1)
+dataAM_1,carrierAM_1,rateAM_1 = modulationAM(data_interpol,rate,1)
 dataDemodulationAM_1 = demodulationAM(dataAM_1,rate,1)
 filtered_1 = filtradoBajo(rate,dataDemodulationAM_1)
-dataFM_1,portadoraFM_1,rateFM_1 = modulationFM(data_interpol,rate,1)
-#graphicAM(data,data_interpol,portadoraAM_1,dataAM_1,"100%",filtered_1)
-#graphicFM(data,data_interpol,portadoraFM_1,dataFM_1,"100%")
-#graphicFourier(data_interpol,rate,dataAM_1,rateAM_1,dataFM_1,rateFM_1,filtered_1,"100%")
-wavfile.write("filtradoBajo.wav",rate,filtered_1)
+dataFM_1,carrierFM_1,rateFM_1 = modulationFM(data_interpol,rate,1)
+graphicAM(data,data_interpol,carrierAM_1,dataAM_1,"100%",filtered_1)
+graphicFM(data,data_interpol,carrierFM_1,dataFM_1,"100%")
+graphicFourier(data_interpol,rate,dataAM_1,rateAM_1,dataFM_1,rateFM_1,filtered_1,carrierAM_1,"100%",1)
+graphicFourier(data_interpol,rate,dataAM_1,rateAM_1,dataFM_1,rateFM_1,filtered_1,carrierAM_1,"100%",0)
+#wavfile.write("filtradoBajo.wav",rate,filtered_1)
 
 
 ####################### MODULACION 15%#######################################
-dataAM_15,portadoraAM_15,rateAM_15 = modulationAM(data_interpol,rate,0.15)
+dataAM_15,carrierAM_15,rateAM_15 = modulationAM(data_interpol,rate,0.15)
 dataDemodulationAM_15 = demodulationAM(dataAM_15,rate,0.15)
 filtered_15 = filtradoBajo(rate,dataDemodulationAM_15)
-dataFM_15,portadoraFM_15,rateFM_15 = modulationFM(data_interpol,rate,0.15)
-#graphicAM(data,data_interpol,portadoraAM_15,dataAM_15,"15%",filtered_15)
-#graphicFM(data,data_interpol,portadoraFM_15,dataFM_15,"15%")
-#graphicFourier(data,rate,dataAM_15,rateAM_15,dataFM_15,rateFM_15,filtered_15,"15%")
-wavfile.write("filtradoBajo2.wav",rate,filtered_15)
+dataFM_15,carrierFM_15,rateFM_15 = modulationFM(data_interpol,rate,0.15)
+graphicAM(data,data_interpol,carrierAM_15,dataAM_15,"15%",filtered_15)
+graphicFM(data,data_interpol,carrierFM_15,dataFM_15,"15%")
+graphicFourier(data,rate,dataAM_15,rateAM_15,dataFM_15,rateFM_15,filtered_15,carrierAM_15,"15%",1)
+graphicFourier(data,rate,dataAM_15,rateAM_15,dataFM_15,rateFM_15,filtered_15,carrierAM_15,"15%",0)
+#wavfile.write("filtradoBajo.wav",rate,filtered_15)
 
 ####################### MODULACION 125%#######################################
-dataAM_125,portadoraAM_125,rateAM_125 = modulationAM(data_interpol,rate,1.25)
+dataAM_125,carrierAM_125,rateAM_125 = modulationAM(data_interpol,rate,1.25)
 dataDemodulationAM_125 = demodulationAM(dataAM_125,rate,1.25)
 filtered_125 = filtradoBajo(rate,dataDemodulationAM_125)
-dataFM_125,portadoraFM_125,rateFM_125 = modulationFM(data_interpol,rate,1.25)
-#graphicAM(data,data_interpol,portadoraAM_125,dataAM_125,"125%",filtered_125)
-#graphicFM(data,data_interpol,portadoraFM_125,dataFM_125,"125%")
-#graphicFourier(data,rate,dataAM_125,rateAM_125,dataFM_125,rateFM_125,filtered_125,"125%")
-wavfile.write("filtradoBajo3.wav",rate,filtered_125)
+dataFM_125,carrier_125,rateFM_125 = modulationFM(data_interpol,rate,1.25)
+graphicAM(data,data_interpol,carrierAM_125,dataAM_125,"125%",filtered_125)
+graphicFM(data,data_interpol,carrier_125,dataFM_125,"125%")
+graphicFourier(data,rate,dataAM_125,rateAM_125,dataFM_125,rateFM_125,filtered_125,carrierAM_125,"125%",1)
+graphicFourier(data,rate,dataAM_125,rateAM_125,dataFM_125,rateFM_125,filtered_125,carrierAM_125,"125%",0)
+#wavfile.write("filtradoBajo.wav",rate,filtered_125)
 
